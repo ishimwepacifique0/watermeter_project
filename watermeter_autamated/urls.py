6a19bd20df7f509fcd2d384a48001bb936e3ec6f
Paccy <ishimwepacifique0@gmail.com>
2022-11-18 01:52:31 -0500

    change

--- watermeter_autamated/urls.py
+++ watermeter_autamated/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include("watermeter.urls")),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
